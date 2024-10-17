e109fd628a40431c4185c7121febfac00ef98d14
nexspherebusiness <nexspherebusiness@gmail.com>
2024-10-16 21:15:51 -0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,4 +2,7 @@
 #!/bin/bash
 
 # Defina as permissões corretas para os arquivos que você precisa
+chmod +x build.sh
+./build.sh && npm run build
+
 chmod +x sendemail.php  # Exemplo: Torna o arquivo executável (se necessário)
